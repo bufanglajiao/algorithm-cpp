fe41e4d35ccb1bf660a2b4c5024806164fca7c30
YinLei <1099129793@qq.com>
2021-08-21 21:41:04 +0800

    C语言

---
+++ HelloWorldC/data_copy/factorial.h
@@ -0,0 +1,11 @@
+//
+// Created by 10991 on 2021/8/20.
+//
+
+#ifndef HELLOWORLDC_HELLOWORLDC_INCLUDE_FACTORIAL_H_
+#define HELLOWORLDC_HELLOWORLDC_INCLUDE_FACTORIAL_H_
+unsigned int Factorial(unsigned int n);
+unsigned int FactorialByIteration(unsigned int n);
+
+
+#endif //HELLOWORLDC_HELLOWORLDC_INCLUDE_FACTORIAL_H_
